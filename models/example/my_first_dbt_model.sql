45ac876993310c2a2f755bd8f93f7cdc8a89009f
ateeq.iqbal@dunelm.com <ateeq.iqbal@dunelm.com>
2021-09-01 16:12:06 +0000

    Test Commit

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -7,8 +7,7 @@
     Try changing "table" to "view" below
 */
 
-{{ config(materialized='table') }}
-     //AteeqTestPR
+{{ config(materialized='view') }}
 with source_data as (
 
     select 1 as id
@@ -19,9 +18,4 @@ with source_data as (
 
 select *
 from source_data
-
-/*
-    Uncomment the line below to remove records with null `id` values
-*/
-
--- where id is not null
+WHERE ID is not null

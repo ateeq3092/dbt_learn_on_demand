3a3dd30592c0f23af218fd5a09ce99e73c000709
ateeq.iqbal@dunelm.com <ateeq.iqbal@dunelm.com>
2021-09-01 13:26:16 +0000

    Commited change 2

--- models/example/ateeq.sql
+++ models/example/ateeq.sql
@@ -9,3 +9,5 @@ select * from AteeqTest.jaffle_shop.orders o
 inner join AteeqTest.jaffle_shop.customers c on o.User_ID = c.ID
 inner join AteeqTest.stripe.payment p on p.OrderID  = o.ID
 WHERE o.ID between 9 and 55
+
+//Change here

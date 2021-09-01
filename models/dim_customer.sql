1411522f0329e114d029e56d8eb75b3fee19cc85
ateeq.iqbal@dunelm.com <ateeq.iqbal@dunelm.com>
2021-09-01 16:27:46 +0000

    Test Commit

--- models/dim_customer.sql
+++ models/dim_customer.sql
@@ -1,4 +1,4 @@
-{{ config(materialized='table') }}
+{{ config(materialized='view') }}
 
 with customers as (
 
@@ -38,22 +38,21 @@ customer_orders as (
 
 ),
 
-
 final as (
 
     select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        co.first_order_date,
+        co.most_recent_order_date,
+        coalesce(co.number_of_orders, 0) as number_of_orders
 
     from customers
 
-    left join customer_orders using (customer_id)
+    left join customer_orders co on co.customer_id = customers.customer_id
 
 )
 
-select * from final
+ select * from final
 
